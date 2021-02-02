d6c10a6a98f6292d303d71a873c1cda1e9154b1e
pokmon987 <pokmon987@gmail.com>
2021-02-02 15:17:41 -0500

    Added the rewind particle
    Can be changed or removed, right now its just used to signify that the unwound jukebox is running

--- src/main/java/net/yuqera/rewind/setup/Registration.java
+++ src/main/java/net/yuqera/rewind/setup/Registration.java
@@ -2,6 +2,7 @@ package net.yuqera.rewind.setup;
 
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.Item;
+import net.minecraft.particles.ParticleType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +13,7 @@ public class Registration {
     public static final RewindItemGroup REWIND_ITEM_GROUP = new RewindItemGroup("rewind");
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RewindMod.MOD_ID);
+    public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, RewindMod.MOD_ID);
     public static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, RewindMod.MOD_ID);
 
     public static void register() {
@@ -20,11 +22,13 @@ public class Registration {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ITEMS.register(modEventBus);
+        PARTICLES.register(modEventBus);
         CONTAINERS.register(modEventBus);
 
         // REGISTER ITEMS/BLOCKS
         RewindMod.LOGGER.info("Registering Mod.");
         ModItems.register();
+        ModParticles.register();
         ModContainers.register();
     }
 }
