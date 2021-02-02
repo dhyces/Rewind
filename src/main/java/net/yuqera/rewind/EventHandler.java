e4d28d3aa476f0cb2c0a2cc3c0446e31cde8b0d0
pokmon987 <pokmon987@gmail.com>
2021-02-02 15:13:56 -0500

    Update EventHandler.java
    Made the break block event return without cancelling should the player not have an active watch.

--- src/main/java/net/yuqera/rewind/EventHandler.java
+++ src/main/java/net/yuqera/rewind/EventHandler.java
@@ -1,44 +1,51 @@
 package net.yuqera.rewind;
 
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.yuqera.rewind.config.RewindConfig;
 import net.yuqera.rewind.item.time_watcher.TimeWatcherItem;
 import net.yuqera.rewind.setup.ModItems;
 import net.yuqera.rewind.utils.BlockHistoryHandler;
 import net.yuqera.rewind.utils.PlayerInventoryUtilities;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
+
+@Mod.EventBusSubscriber(bus = Bus.FORGE, modid = RewindMod.MOD_ID)
 public class EventHandler {
 
     @SubscribeEvent
-    public static void breakBlock(BlockEvent.BreakEvent event) {
+    public static void breakBlock(final BlockEvent.BreakEvent event) {
         // GATES
         if (event.isCanceled() || event.getResult() == Event.Result.DENY)
             return;
 
-        event.setCanceled(true);
-        BlockHistoryHandler.pushBlockHistory(event.getPlayer().getHeldItemMainhand(), event.getState().getBlock(), event.getPos());
-        event.getWorld().removeBlock(event.getPos(), false);
-        System.out.println();
         ArrayList<ItemStack> foundWatches = PlayerInventoryUtilities.getAllItemStacksWhere(event.getPlayer().inventory, ModItems.TIME_WATCHER.get());
-        System.out.println(getFirstActiveWatch(foundWatches).getTag());
+        ItemStack watch = getFirstActiveWatch(foundWatches);
+        if (watch.isEmpty()) return;
+        event.setCanceled(true);
+        BlockHistoryHandler.pushBlockHistory(watch, event.getState().getBlock(), event.getPos(), event.getPlayer().getEntityWorld());
+        event.getWorld().removeBlock(event.getPos(), false);
+        System.out.println("\n" + getFirstActiveWatch(foundWatches).getTag());
 
     }
+    
+    // TODO: Remove, as it doesn't fit how the watches will function. Only one watch should be active at a time
+    private static Optional<List<ItemStack>> getNonFullActiveWatches(List<ItemStack> watches) {
+        return Optional.of(watches).filter(c -> c.stream().anyMatch(x -> {
+        	return TimeWatcherItem.getActivationState(x).getState() && BlockHistoryHandler.getTimeWatchHistoryCount(x) < RewindConfig.rewind_max_size.get();
+        }));
+    }
 
-    private static ItemStack getFirstActiveWatch(ArrayList<ItemStack> watches) {
-        ItemStack firstActiveWatch = new ItemStack(Items.AIR, 1);
+    private static ItemStack getFirstActiveWatch(List<ItemStack> watches) {
+    	ItemStack firstActiveWatch = ItemStack.EMPTY;
         for (ItemStack watch : watches) {
             if (watch.getOrCreateTag().getFloat(TimeWatcherItem.NBT_TAG_NAME_ACTIVATION) != 0.0F) {
                 firstActiveWatch = watch;
